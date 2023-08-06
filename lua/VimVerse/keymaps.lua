cd1a216fbc11539d7210e07f7c28b91dd02987d4
Aryan Thakur <aryan197297@gmail.com>
2023-08-06 15:31:05 +0530

    feat: add the keymaps

---
+++ lua/VimVerse/keymaps.lua
@@ -0,0 +1,35 @@
+local opts = { noremap = true, silent = true }
+local termp_opts = { silent = true }
+
+-- Make Easily Accessible
+local keymap = vim.keymap.set
+
+-- Space is the leader
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- REMAPPING IN NORMAL MODE --
+-- fast buffer navigation
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- REMAPPING IN INSERT MODE --
+keymap("i", "jk", "<ESC>", opts)
+
+-- REMAPPING IN VISUAL MODE --
+-- indentation
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- visual move up and down
+keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+keymap("v", "p", '"_dP', opts)
+
+-- REMAPPING IN VISUAL BLOCK MODE --
+keymap("x", "J", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+

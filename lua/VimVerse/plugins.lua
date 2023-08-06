df76af0587146da1a7f317ae7e0e9d2bbecfb1f0
Aryan Thakur <aryan197297@gmail.com>
2023-08-06 17:15:38 +0530

    feat: add plugins

--- lua/VimVerse/plugins.lua
+++ lua/VimVerse/plugins.lua
@@ -32,7 +32,7 @@ end
 packer.init {
   display = {
     open_fn = function()
-      return require("packer.util").float{ border = "rounded"}
+      return require("packer.util").float{ border = "rounded" }
     end,
   },
 }
@@ -43,6 +43,9 @@ return packer.startup(function(use)
   use "wbthomason/packer.nvim" -- Have packer manage itself
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+
+  -- Styling 
+  use "lunarvim/darkplus.nvim"
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins

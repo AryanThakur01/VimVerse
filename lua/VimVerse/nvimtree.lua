fafa2c2731aca537117cccb1141ab1c8d8358004
Aryan Thakur <aryan197297@gmail.com>
2023-08-13 11:04:08 +0530

    feat: add the nvim tree

---
+++ lua/VimVerse/nvimtree.lua
@@ -0,0 +1,27 @@
+-- empty setup using defaults
+local status_ok, nvim_tree = pcall(require,"nvim-tree")
+if not status_ok then
+  print("nvim-tree Not Working")
+  return
+end
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+-- OR setup with some options
+nvim_tree.setup({
+  sort_by = "case_sensitive",
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
--- lua/VimVerse/plugins.lua
+++ lua/VimVerse/plugins.lua
@@ -45,6 +45,8 @@ return packer.startup(function(use)
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
   use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter
   use "numToStr/Comment.nvim" -- Easily Comment Stuffs
+  use 'kyazdani42/nvim-web-devicons'
+  use 'kyazdani42/nvim-tree.lua'
 
   -- Styling 
   -- use "lunarvim/darkplus.nvim"
